c6dab19e140d97e08a7102c98470f1c29ab70bff
Prathamesh Tatar <prathameshtatar@gmail.com>
2025-10-21 22:07:53 +0530

    Pattern 04

---
+++ 000 Patterns/Pattern 04.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+#include "../utils/utils.h"
+
+using namespace std;
+
+class Solution {
+public:
+    void pattern4(int n) {
+      for (int i = 1; i <= n; ++i) {
+        for (int j = 1; j <= i; ++j) {
+          cout << i;
+        }
+        cout << "\n";
+      }
+    }
+};
+
+int main() {
+  Solution s;
+  int n = readInt();
+  cout << n << "\n";
+  readNLines(n, [&s](const string& line) {
+    int k = stoi(line);
+    cout << "\n"
+         << k << "\n";
+    s.pattern4(k);
+  });
+  return 0;
+}
