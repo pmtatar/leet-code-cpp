041b71f1f751c54416348064983db0370809afe3
Prathamesh Tatar <prathameshtatar@gmail.com>
2025-10-21 22:32:21 +0530

    Pattern 06

---
+++ 000 Patterns/Pattern 06.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+#include "../utils/utils.h"
+
+using namespace std;
+
+class Solution {
+public:
+    void pattern6(int n) {
+      for (int i = 0; i < n; ++i) {
+        for (int j = 1; j <= (n - i); ++j) {
+          cout << j;
+        }
+        cout << "\n";
+      }
+    }
+};
+
+int main() {
+  Solution s;
+  int n = readInt();
+  cout << n << "\n";
+  readNLines(n, [&s](const string& line) {
+    int k = stoi(line);
+    cout << "\n"
+         << k << "\n";
+    s.pattern6(k);
+  });
+  return 0;
+}
