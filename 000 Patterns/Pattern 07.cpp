89aa8c9daa237c2f7c6af229bfd77683c1d8f4c7
Prathamesh Tatar <prathameshtatar@gmail.com>
2025-10-21 22:46:43 +0530

    Pattern 07

---
+++ 000 Patterns/Pattern 07.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+#include "../utils/utils.h"
+
+using namespace std;
+
+class Solution {
+public:
+    void pattern7(int n) {
+      for (int i = 1; i <= n; ++i) {
+        for (int j = 1; j <= (n - i); ++j) {
+          cout << " ";
+        }
+        for (int j = 0; j < ((i * 2) - 1); ++j) {
+          cout << "*";
+        }
+        cout << "\n";
+      }
+    }
+};
+
+int main() {
+  Solution s;
+  int n = readInt();
+  cout << n << "\n";
+  readNLines(n, [&s](const string& line) {
+    int k = stoi(line);
+    cout << "\n"
+         << k << "\n";
+    s.pattern7(k);
+  });
+  return 0;
+}
