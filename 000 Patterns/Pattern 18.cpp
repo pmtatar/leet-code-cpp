e6671007ad215f034f1211c6b5b402cc49a275b7
Prathamesh Tatar <prathameshtatar@gmail.com>
2025-10-22 02:19:59 +0530

    Pattern 18

---
+++ 000 Patterns/Pattern 18.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+#include "../utils/utils.h"
+
+using namespace std;
+
+class Solution {
+public:
+    void pattern18(int n) {
+      for (int i = 0; i < n; ++i) {
+        for (int j = i; j >= 0; --j) {
+          cout << char('A' + n - j - 1);
+          if (j > 0) {
+            cout << " ";
+          }
+        }
+        cout << "\n";
+      }
+    }
+};
+
+int main() {
+  Solution s;
+  int n = readInt();
+  cout << n << "\n";
+  readNLines(n, [&s](const string& line) {
+    int k = stoi(line);
+    cout << "\n"
+         << k << "\n";
+    s.pattern18(k);
+  });
+  return 0;
+}
