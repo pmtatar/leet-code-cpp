9375f747ab61fdaff390c93dea977eff1c159dbc
Prathamesh Tatar <prathameshtatar@gmail.com>
2025-10-22 01:18:39 +0530

    Pattern 14

---
+++ 000 Patterns/Pattern 14.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+#include "../utils/utils.h"
+
+using namespace std;
+
+class Solution {
+public:
+  void pattern14(int n) {
+    for (int i = 0; i < n; ++i) {
+      for (int j = 0; j <= i; ++j) {
+        cout << char('A' + j);
+      }
+      cout << "\n";
+    }
+  }
+};
+
+int main() {
+  Solution s;
+  int n = readInt();
+  cout << n << "\n";
+  readNLines(n, [&s](const string& line) {
+    int k = stoi(line);
+    cout << "\n"
+         << k << "\n";
+    s.pattern14(k);
+  });
+  return 0;
+}
