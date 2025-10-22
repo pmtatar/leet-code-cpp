30d9dbd80897fd7c416bf4ccd2d5e48d93f82ca3
Prathamesh Tatar <prathameshtatar@gmail.com>
2025-10-22 10:38:13 +0530

    Pattern 19

---
+++ 000 Patterns/Pattern 19.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+#include "../utils/utils.h"
+
+using namespace std;
+
+class Solution {
+public:
+  void pattern19(int n) {
+    // Upper half of the pattern
+    for (int i = 0; i < n; ++i) {
+      for (int j = 0; j < n - i; ++j) {
+        cout << "*";
+      }
+      for (int j = 0; j < i * 2; ++j) {
+        cout << " ";
+      }
+      for (int j = 0; j < n - i; ++j) {
+        cout << "*";
+      }
+      cout << "\n";
+    }
+    // Lower half of the pattern
+    for (int i = 0; i < n; ++i) {
+      for (int j = 0; j <= i; ++j) {
+        cout << "*";
+      }
+      for (int j = 0; j < (n - i - 1) * 2; ++j) {
+        cout << " ";
+      }
+      for (int j = 0; j <= i; ++j) {
+        cout << "*";
+      }
+      cout << "\n";
+    }
+  }
+};
+
+int main() {
+  Solution s;
+  int n = readInt();
+  cout << n << "\n";
+  readNLines(n, [&s](const string& line) {
+    int k = stoi(line);
+    cout << "\n"
+         << k << "\n";
+    s.pattern19(k);
+  });
+  return 0;
+}
